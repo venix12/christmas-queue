6a197761c1fb2179f3d4213da7fc25d271ad461e
venix12 <chopek.micha@gmail.com>
2019-11-07 20:28:32 +0100

    don't let user request a refused beatmap again
    Fixes #3

--- app/Http/Controllers/BeatmapsetController.php
+++ app/Http/Controllers/BeatmapsetController.php
@@ -19,20 +19,26 @@ class BeatmapsetController extends Controller
     {
         $maps = Mapset::all();
 
-        $notApproved = count($maps->where('approved', 0)->where('user_id', Auth::id()));
+        $alreadyDeleted = count($maps->where('deleted', 1)->where('beatmapset_osu_id', $request->beatmapsetId));
+        $notApproved = count($maps->where('deleted', 0)->where('approved', 0)->where('user_id', Auth::id()));
 
         if($notApproved < 1)
         {
-            $mapset = Mapset::create([
-                'user_id' => Auth::id(),
-                'osu_user_id' => $request->osuUserId,
-                'beatmapset_artist' => $request->beatmapsetArtist,
-                'beatmapset_creator' => $request->beatmapsetCreator,
-                'beatmapset_osu_id' => $request->beatmapsetId,
-                'beatmapset_title' => $request->beatmapsetTitle,
-            ]);
+            if($alreadyDeleted < 1)
+            {
+                $mapset = Mapset::create([
+                    'user_id' => Auth::id(),
+                    'osu_user_id' => $request->osuUserId,
+                    'beatmapset_artist' => $request->beatmapsetArtist,
+                    'beatmapset_creator' => $request->beatmapsetCreator,
+                    'beatmapset_osu_id' => $request->beatmapsetId,
+                    'beatmapset_title' => $request->beatmapsetTitle,
+                ]);
+            } else {
+                $mapset = ['error' => 'This beatmap was already refused!'];
+            }
         } else {
-            $mapset = ['error' => 'You already requested a map awaiting approval!'];
+            $mapset = ['error' => 'You already requested a beatmap awaiting approval!'];
         }
 
         return response()->json($mapset);
