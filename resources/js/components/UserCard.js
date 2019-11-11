6fdfd695c9c6884a64b1690e60820a0e33966c19
venix12 <chopek.micha@gmail.com>
2019-11-11 14:47:03 +0100

    minor consistency fixes

--- resources/js/components/UserCard.js
+++ resources/js/components/UserCard.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class UserCard extends Component {
     render() {
+        const { osu_id, username, user_title} = this.props;
         return (
             <div class="user-card round-text text-left">
-                <img class="avatar float-left" src={`https://a.ppy.sh/${this.props.osu_id}`}></img>
-                {this.props.username}<br></br>
-                <span class={`color-${this.props.user_title}`}>{this.props.user_title}</span>
+                <img class="avatar float-left" src={`https://a.ppy.sh/${osu_id}`}></img>
+                {username}<br></br>
+                <span class={`color-${user_title.toLowerCase()}`}>{user_title}</span>
             </div>
         );
     }
