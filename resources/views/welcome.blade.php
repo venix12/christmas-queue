8e4b24eeec4b4547643edf34622e9282943aae85
venix12 <chopek.micha@gmail.com>
2019-11-14 18:39:18 +0100

    use proper discord link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <div class="text-field" style="margin-bottom: 10px">
         Once again this year,  the Christmas Queue continues for its ninth year of operation, a tradition that continues to spread the joy and wonders of the winter. Like before, in order to make this a great success again, the Christmas Queue is in need of motivated individuals to help push things forward. This year, we will make at least one beatmap pack and maybe even a news post about the newly ranked beatmaps that will be released just before Christmas.
         <br><br>
-        Join us on <a href="" style="text-decoration: none !important">Discord</a>!
+        Join us on <a href="https://discord.gg/782TTSN" style="text-decoration: none !important">Discord</a>!
         <br><br>
         If you aren't listed as a modder on this website and want to be one, contact an ambassador to get added!
     </div><br>
