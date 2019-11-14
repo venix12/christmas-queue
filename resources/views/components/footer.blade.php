35b326b5ac39b6308f17785fcaeaade49cdfc9a9
venix12 <chopek.micha@gmail.com>
2019-11-14 16:23:26 +0100

    use proper github repo link in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer" style="color: white">
     <div class="container">
-        <span class="float-left">Christmas Queue 2019&nbsp;&nbsp;&nbsp; <i class="fa fa-github"></i> <a class="text-white" href="https://github.com/venix12/spotlights">GitHub</a></span>
+        <span class="float-left">Christmas Queue 2019&nbsp;&nbsp;&nbsp; <i class="fa fa-github"></i> <a class="text-white" href="https://github.com/venix12/christmas-queue">GitHub</a></span>
         <span class="float-right">Created by Venix</span>
     </div>
 </footer>
