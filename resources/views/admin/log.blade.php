7b9a177e011c2219c0b87839c91a0472dac4c5f1
venix12 <chopek.micha@gmail.com>
2019-12-07 17:14:45 +0100

    add missing class to the log

--- resources/views/admin/log.blade.php
+++ resources/views/admin/log.blade.php
@@ -8,24 +8,26 @@
 
     @include('components.session')
 
-    @foreach($events as $event)
-        <div class="user-listing__bg">
-            <div class="user-listing__card">
-                <div>
+    <div class="user-listing">
+        @foreach($events as $event)
+            <div class="user-listing__bg">
+                <div class="user-listing__card">
                     <div>
-                        <img class="user-listing__avatar" src="https://a.ppy.sh/{{$event->user->osu_id}}">
-                        <a href="https://osu.ppy.sh/u/{{$event->user->osu_id}}" class="user-listing__el user-listing__el--link">{{$event->user->username}}</a>
-                        <span class="user-listing__el user-listing__el--content">
-                            {{$event->action}}
-                        </span>
+                        <div>
+                            <img class="user-listing__avatar" src="https://a.ppy.sh/{{$event->user->osu_id}}">
+                            <a href="https://osu.ppy.sh/u/{{$event->user->osu_id}}" class="user-listing__el user-listing__el--link">{{$event->user->username}}</a>
+                            <span class="user-listing__el user-listing__el--content">
+                                {{$event->action}}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="user-listing__el user-listing__el--content">
+                        {{substr($event->created_at, 0, 16)}}
                     </div>
                 </div>
-                <div class="user-listing__el user-listing__el--content">
-                    {{substr($event->created_at, 0, 16)}}
-                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 
     {{ $events->links('components.paginator') }}
 @endsection
