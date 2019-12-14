96834e3c1b70265f32d474b441d6c8cef2cd71ab
clayton <me@clayton.cc>
2019-12-14 15:38:47 -0800

    Add methods for creating and updating mapsets via API

--- app/Mapset.php
+++ app/Mapset.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OsuApi;
 
 class Mapset extends Model
 {
@@ -36,4 +37,53 @@ class Mapset extends Model
         'taiko',
         'user_id'
     ];
+
+    public static function createFromOnline(int $beatmapsetId, User $user)
+    {
+        $apiBeatmaps = OsuApi::getBeatmapset($beatmapsetId);
+        $mapset = new static();
+
+        if ($mapset->fillOnline($apiBeatmaps)) {
+            $mapset->user_id = $user->id;
+            $mapset->save();
+
+            return $mapset;
+        }
+    }
+
+    public function updateOnline()
+    {
+        $apiBeatmaps = OsuApi::getBeatmapset($this->beatmapset_osu_id);
+
+        if ($this->fillOnline($apiBeatmaps)) {
+            $this->save();
+        }
+    }
+
+    private function fillOnline(array $apiBeatmaps) : bool
+    {
+        if (count($apiBeatmaps) === 0) {
+            return false;
+        }
+
+        $modes = [false, false, false, false];
+
+        foreach ($apiBeatmaps as $beatmap) {
+            $modes[$beatmap->mode] = true;
+        }
+
+        $this->fill([
+            'osu_user_id' => $apiBeatmaps[0]->creator_id,
+            'beatmapset_artist' => $apiBeatmaps[0]->artist,
+            'beatmapset_creator' => $apiBeatmaps[0]->creator,
+            'beatmapset_osu_id' => $apiBeatmaps[0]->beatmapset_id,
+            'beatmapset_title' => $apiBeatmaps[0]->title,
+            'osu' => $modes[0],
+            'taiko' => $modes[1],
+            'catch' => $modes[2],
+            'mania' => $modes[3],
+        ]);
+
+        return true;
+    }
 }
