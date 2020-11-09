1f0b6664887b63dc12a88a6f1408639a7d77b6db
Venix <30481900+venix12@users.noreply.github.com>
2020-11-09 17:37:26 +0100

    update footer
    fixes #31

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer" style="color: white">
     <div class="container">
         <span class="float-left">
-            <a class="text-white" href="https://osu.ppy.sh/community/forums/topics/985375">Christmas Queue 2019</a>&nbsp;&nbsp;&nbsp;
+            <a class="text-white" href="https://osu.ppy.sh/community/forums/topics/1175112">Christmas Queue 2020</a>&nbsp;&nbsp;&nbsp;
             <i class="fa fa-github"></i> <a class="text-white" href="https://github.com/venix12/christmas-queue">GitHub</a>
         </span>
         <span class="float-right">Created by Venix</span>
